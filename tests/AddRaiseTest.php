fc513e8a34ce6e9a5ef170499a78a93d896813dd
Woeffler Maxime <maxime14mars@gmail.com>
2023-11-23 16:53:52 +0100

    add panther test

---
+++ tests/AddRaiseTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Auction;
+use App\Enum\Status;
+use App\Repository\AuctionRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Panther\PantherTestCase;
+
+class AddRaiseTest extends PantherTestCase
+{
+    public function test_add_raise(): void
+    {
+        $client = static::createPantherClient(['external_base_uri' => 'http://localhost:8000']);
+        $crawler = $client->request('GET', '/');
+        $client->waitForElementToContain('h1', 'Auction List', 10);
+
+        $entityManager = static::getContainer()->get(EntityManagerInterface::class);
+        $auction = new Auction();
+        $auction->setTitle('Auction');
+        $auction->setPrice(500);
+        $auction->setDescription('description');
+        $auction->setDateOpen((new DateTime())->modify('-1 day'));
+        $auction->setDateClose((new DateTime())->modify('+1 day'));
+        $auction->setImage('image');
+        $auction->setStatus(Status::VISIBLE);
+        $entityManager->persist($auction);
+        $entityManager->flush();
+
+        $client->waitForElementToContain('#auction'.$auction->getId().' .card-text > p:nth-child(1)', '5,00 €', 10);
+        $form = $crawler->filter('#auction'.$auction->getId().' form')->form([
+            "price" => 10
+        ]);
+        $client->submit($form);
+        $client->waitForElementToContain('#auction'.$auction->getId().' .card-text > p:nth-child(1)', '10,00 €', 10);
+        $client->waitForElementToContain('#auction'.$auction->getId().' .alert-success', 'Raise successfully created', 10);
+        $client->takeScreenshot(__DIR__ . '/screens/accueil.png');
+    }
+}
