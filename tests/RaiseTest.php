89ff7b255d022162eefbd0b0205c691da6cda5d2
Woeffler Maxime <maxime14mars@gmail.com>
2023-11-23 12:29:58 +0100

    add last raise test

--- tests/RaiseTest.php
+++ tests/RaiseTest.php
@@ -4,26 +4,32 @@ namespace App\Tests;
 
 use App\Entity\Auction;
 use App\Entity\Raise;
-use App\Repository\RaiseRepository;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class RaiseTest extends KernelTestCase
 {
     public const SHOULD_BE_POSITIVE_ERROR = 'This value should be positive.';
     public const SHOULD_BE_ENOUGH = 'Value must be greater than highest raise';
+    protected ValidatorInterface $validator;
+    protected ContainerInterface $container;
+
+    protected function setUp(): void
+    {
+        $this->raise = $this->createMock(Raise::class);
+        $this->container = self::getContainer();
+        $this->validator = $this->container->get(ValidatorInterface::class);
+    }
 
     public function test_price_should_not_be_negative(): void
     {
         $kernel = self::bootKernel();
-        $container = static::getContainer();
-
         $this->assertSame('test', $kernel->getEnvironment());
-        $validator = $container->get(ValidatorInterface::class);
 
         $raise = new Raise();
         $raise->setPrice(-10);
-        $errors = $validator->validate($raise);
+        $errors = $this->validator->validate($raise);
         $this->assertNotEmpty($errors);
         $this->assertCount(2, $errors);
         $this->assertStringContainsString(self::SHOULD_BE_POSITIVE_ERROR, $errors);
@@ -33,14 +39,11 @@ class RaiseTest extends KernelTestCase
     public function test_price_should_be_enough(): void
     {
         $kernel = self::bootKernel();
-        $container = static::getContainer();
-
         $this->assertSame('test', $kernel->getEnvironment());
-        $validator = $container->get(ValidatorInterface::class);
 
         $raise = new Raise();
         $raise->setPrice(4);
-        $errors = $validator->validate($raise);
+        $errors = $this->validator->validate($raise);
         $this->assertNotEmpty($errors);
         $this->assertCount(1, $errors);
         $this->assertStringContainsString(self::SHOULD_BE_ENOUGH, $errors[0]->getMessage());
@@ -49,15 +52,23 @@ class RaiseTest extends KernelTestCase
     public function test_price_should_be_valid(): void
     {
         $kernel = self::bootKernel();
-        $container = static::getContainer();
-
         $this->assertSame('test', $kernel->getEnvironment());
-        $validator = $container->get(ValidatorInterface::class);
 
         $raise = new Raise();
         $raise->setPrice(5 * 100);
-        $errors = $validator->validate($raise);
+        $errors = $this->validator->validate($raise);
         $this->assertEmpty($errors);
         $this->assertCount(0, $errors);
+    }
+
+    public function test_last_raise(): void
+    {
+        $this->assertSame('test', self::bootKernel()->getEnvironment());
+        $auction = new Auction();
+        $auction->addRaise($this->raise);
+        $this->raise->expects($this->once())
+            ->method('getLastRaise')
+            ->willReturn($auction->getRaises()->last());
+        $this->assertEquals($this->raise, $this->raise->getLastRaise());
     }
 }
